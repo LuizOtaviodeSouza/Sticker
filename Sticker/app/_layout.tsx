7e67a38ae5da76ce1068ac953428b30f26b50ce3
Luiz <lui-otavio@hotmail.com>
2025-02-27 10:49:55 -0300

    sad

--- Sticker/app/_layout.tsx
+++ Sticker/app/_layout.tsx
@@ -1,4 +1,5 @@
 import { Stack } from "expo-router";
+import Ionincons from "@/vector-icons/"
 
 export default function RootLayout() {
   return(
--- Sticker/app/index.tsx
+++ Sticker/app/index.tsx
@@ -1,15 +1,30 @@
 import { Text, View } from "react-native";
+import { image } from 'empo-image'
+
+const PlaceholderImage = require('@/assets/images/background-image.png');
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style = {styles.container}>
+      <View styles={styles.imageContainer}>
+        <Image sourcce={PlaceholderImage} style={StyleSheet.image} />
+        </View>
     </View>
   );
 }
+
+const styles = StylesSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#25592e",
+    alingItems: "center",
+  },
+  imageContainer: {
+    flex: 1,
+  },
+  image: {
+    width: 320,
+    height: 400,
+    borderRadius: 18
+  },
+});
